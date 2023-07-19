f15296f5a3a397fb2aacbdc38240a02412578704
nickwbarber <nickwbarber@gmail.com>
2023-07-19 16:00:39 +0800

    Add login form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import Login from "./components/Login";
 import blogService from "./services/blogs";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [notificationMessage, setNotificationMessage] = useState(null);
   const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -11,6 +15,15 @@ const App = () => {
 
   return (
     <div>
+      { errorMessage ? <p>{errorMessage}</p> : null }
+      { notificationMessage ? <p>{notificationMessage}</p> : null }
+      { user === null
+        ? <Login
+          setUser={setUser}
+          setErrorMessage={setErrorMessage}
+        />
+        : <p>{user.name} logged in</p>
+      }
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import loginService from "../services/login";
+
+const loginWithUsernameAndPassword = async (username, password) => {
+  const user = await loginService.login({
+    username,
+    password,
+  });
+  window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+  return user;
+}
+
+const Login = ({ setUser, setErrorMessage }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginWithUsernameAndPassword(username, password);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (err) {
+      setErrorMessage("Wrong credentials");
+      setPassword("");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username{" "}
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password{" "}
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+};
+
+export default Login;
