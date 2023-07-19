3fd7caa60f9584e7d6a2de58b24a1791535321bf
nickwbarber <nickwbarber@gmail.com>
2023-07-19 16:00:25 +0800

    Remove useless comment

--- src/services/login.js
+++ src/services/login.js
@@ -5,6 +5,5 @@ const login = async (credentials) => {
   return axios.post(baseUrl, credentials);
 };
 
-// export default login; // NOTE: does the default export have to be an object?
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { login };
