04665fff30e96593b6013d7ed0062a6d90788e62
nickwbarber <nickwbarber@gmail.com>
2023-07-25 17:16:34 +0800

    Write some E2E tests

--- cypress/e2e/blogapp_spec.cy.js
+++ cypress/e2e/blogapp_spec.cy.js
@@ -1,5 +1,26 @@
+const baseUrl = "http://localhost:3000";
+const serverUrl = "http://localhost:3003";
+
 describe("Blog app", () => {
+  beforeEach(() => {
+    // reset db
+    cy.request("POST", `${serverUrl}/api/testing/reset`);
+
+    // create a test user
+    const user = {
+      name: `testuser-${Date.now()}}`,
+      username: `testusername-${Date.now()}}`,
+      password: `testpassword-${Date.now()}}`,
+    };
+    cy.request("POST", `${serverUrl}/api/users/`, user);
+
+    cy.visit(baseUrl);
+  });
+
   it("front page can be opened", () => {
-    cy.visit("http://localhost:3003");
+    cy.contains("Blog App");
+    cy.get("login").click();
+    cy.get("#username").type("test");
+    cy.get("#password").type("test");
   });
 });
