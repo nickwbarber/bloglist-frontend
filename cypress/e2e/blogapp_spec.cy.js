f803a4d4b91d25a4bd4b63c239d7db54f33a841e
nickwbarber <nickwbarber@gmail.com>
2023-07-25 17:53:16 +0800

    Test that login form is shown

--- cypress/e2e/blogapp_spec.cy.js
+++ cypress/e2e/blogapp_spec.cy.js
@@ -1,26 +1,40 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 const baseUrl = "http://localhost:3000";
 const serverUrl = "http://localhost:3003";
 
-describe("Blog app", () => {
-  beforeEach(() => {
+describe("Blog app", function() {
+  // test user config
+  const user = {
+    name: `testuser-${Date.now()}`,
+    username: `testusername-${Date.now()}`,
+    password: `testpassword-${Date.now()}`,
+  };
+
+  beforeEach(function() {
     // reset db
-    cy.request("POST", `${serverUrl}/api/testing/reset`);
-
+    cy.request("POST", `${serverUrl}/api/testing/reset`, { numOfUsers: 2, numOfBlogs: 3 });
     // create a test user
-    const user = {
-      name: `testuser-${Date.now()}}`,
-      username: `testusername-${Date.now()}}`,
-      password: `testpassword-${Date.now()}}`,
-    };
     cy.request("POST", `${serverUrl}/api/users/`, user);
-
+    // visit the app
     cy.visit(baseUrl);
   });
 
-  it("front page can be opened", () => {
+  it("login form is shown", () => {
     cy.contains("Blog App");
-    cy.get("login").click();
-    cy.get("#username").type("test");
-    cy.get("#password").type("test");
+    cy.contains("login").click();
+    cy.get("#username");
+    cy.get("#password");
+  });
+
+  describe("when logging in", function() {
+    // it("login form is shown", () => {
+    //   cy.contains("Blog App");
+    //   cy.contains("login").click();
+    //   cy.get("#username").type(user.username);
+    //   cy.get("#password").type(user.password);
+    //   cy.get("#login-button").click();
+    // });
   });
 });
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,7 +41,7 @@ function Login({ login }) {
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   );
